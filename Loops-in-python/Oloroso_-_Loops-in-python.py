ea489ca4c58b4a237d539e2a077256671b7adc37
ChugxScript <arw.oloroso@gmail.com>
2024-04-17 14:16:54 +0800

    Loops in python

---
+++ Loops-in-python/Oloroso_-_Loops-in-python.py
@@ -0,0 +1,136 @@
+# # for LOOP - range KEYBOARD
+# # Generating even numbers
+# def for_loop_example():
+#     n=int(input("Enter number:"))
+#     for i in range(0,n,2):
+#         print(i)
+# for_loop_example()    
+
+
+
+# # else in the for loop
+# for x in range(6):
+#     print(x)
+# else:
+#     print("Finally finished!")
+
+
+
+
+# # The range() function
+# for x in range(2, 30, 3):
+#     print(x)
+
+
+
+
+
+# # The range() function
+# for x in range(2, 6):
+#     print(x)
+    
+
+
+
+
+# # The range() function
+# for x in range(6):
+#     print(x)
+
+
+
+
+
+
+# # Looping through a string
+# # continue statement
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#     if x == "banana":
+#         continue
+#     print(x)
+
+
+
+# # Looping through a string
+# # brake statement
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#     print(x)
+#     if x == "banana":
+#         break
+
+
+# # Looping through a string
+# for x in "banana":
+#     print(x)
+
+
+# # for LOOP
+# numbers = [1,2,3,4,5]
+# for x in numbers:
+#     print(x)
+
+
+# # for LOOP
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#     print(x)
+
+
+
+
+# # Program to add natural numbers up to
+# # sum = 1+2+3+...+n
+# n=int(input("Enter n:"))
+# sum = 0
+# i = 1
+# while i <= n:
+#     sum = sum + i
+#     i = i+1
+
+# print("The sum is", sum)
+
+
+
+
+# # Using else statement
+# i = 1
+# while i < 6:
+#     print(i)
+#     i += 1
+# else:
+#     print("i is no longer less than 6")
+
+
+
+# # Using continue statement
+# i = 0
+# while i < 6:
+#     i += 1
+#     if i == 3:
+#         continue
+#     print(i)
+
+
+
+
+# # Using break statement
+# i = 1
+# while i < 6:
+#     print(i)
+#     if i == 3:
+#         break
+#     i += 1
+
+
+
+
+
+
+# i = 1
+# n = int(input("Enter a number:"))
+
+# while(i<=n):
+#     print(i)
+#     i+=1
